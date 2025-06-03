9c091f81d570e6a4acdafc66e0121db69cd5be26
AndresGascon <andresgascon21@gmail.com>
2025-06-03 19:53:24 +0200

    v0.1

--- calendar_reader.py
+++ calendar_reader.py
@@ -3,19 +3,17 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from datetime import datetime, timedelta
 import os
+import json
 
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 def autenticar_calendar():
     creds = None
-    if os.path.exists('token_calendar.json'):
-        creds = Credentials.from_authorized_user_file('token_calendar.json', SCOPES)
-    else:
-        flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-        creds = flow.run_local_server(port=0)
-        with open('token_calendar.json', 'w') as token:
-            token.write(creds.to_json())
+    creds_data = json.loads(os.environ["GOOGLE_CREDS"])
+    flow = InstalledAppFlow.from_client_config(creds_data, SCOPES)
+    creds = flow.run_console()  # sin navegador, copia/pega código desde consola de Render
     return build('calendar', 'v3', credentials=creds)
+
 
 def obtener_eventos_semana():
     service = autenticar_calendar()
